7ce23eb5caf817b1fe8350565673bf125774afaf
LukaVDB <lukavdboomen@icloud.com>
2023-03-31 14:26:09 -0700

    345678

--- pizzas.py
+++ pizzas.py
@@ -20,3 +20,42 @@ class Pizzas(db.Model):
 
         self.pizza = pizza
         self.pizzaPrice = pizzaPrice
+
+
+    @property
+    def pizza(self):
+        return self.pizza
+    
+    @pizza.setter
+    def pizza(self, pizza):
+        self.pizza = pizza
+
+    
+    @property
+    def pizzaPrice(self):
+        return self.stock
+    
+    @pizzaPrice.setter
+        def pizzaPrice(self, pizzaPrice):
+        self.pizzaPrice = pizzaPrice
+    
+
+    def __str__(self):
+        return json.dumps(self,read())
+    
+    def create(self):
+        try:
+            db.session.ass(self)
+            db.session.commit()
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+        
+    def read(self):
+        return {
+            "pizza": self.pizza,
+            "pizzaPrice": self.pizzaPrice,
+        }
+    def update(self, pizza="", pizzaPrice=""):
+        
