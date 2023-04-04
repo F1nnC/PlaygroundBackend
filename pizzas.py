00448366d7882ccff49b420f61edcc126be69b38
EdwinKuttappi <edwinab0007@gmail.com>
2023-04-04 13:54:25 -0700

    pizzas changes

--- pizzas.py
+++ pizzas.py
@@ -12,16 +12,15 @@ class Pizzas(db.Model):
     __tablename__ = 'pizzas' # table plural
 
     id = db.Column(db.Integer, primary_key=True)
-    pizza = db.column(db.String(255), unique=False, nullable=False)
-    pizzaPrice = db.column(db.string(255), unique=False, nullable=False)
-    pizzaSize = db.column(db.string(255), unique=False, nullable=False)
+    pizza = db.Column(db.String(255), unique=False, nullable=False)
+    pizzaPrice = db.Column(db.string(255), unique=False, nullable=False)
+    pizzaSize = db.Column(db.string(255), unique=False, nullable=False)
 
 
-    def __init__(self, pizza, pizzaPrice):
-
+    def __init__(self, pizza, pizzaPrice, pizzaSize):
         self.pizza = pizza
         self.pizzaPrice = pizzaPrice
-
+        self.pizzaSize = pizzaSize
 
     @property
     def pizza(self):
@@ -31,7 +30,6 @@ class Pizzas(db.Model):
     def pizza(self, pizza):
         self.pizza = pizza
 
-    
     @property
     def pizzaPrice(self):
         return self.pizzaPrice
@@ -39,6 +37,9 @@ class Pizzas(db.Model):
     @pizzaPrice.setter
     def pizzaPrice(self, pizzaPrice):
             self.pizzaPrice = pizzaPrice
+
+    def is_pizzaPrice(self, pizzaPrice):
+        return self.pizzaPrice == pizzaPrice
     
     @property
     def pizzaSize(self):
@@ -48,7 +49,6 @@ class Pizzas(db.Model):
     def pizzaSize(self, pizzaSize):
         self.pizzasize = pizzaSize
     
-
     def __str__(self):
         return json.dumps(self.read())
     
@@ -67,6 +67,7 @@ class Pizzas(db.Model):
             "pizzaPrice": self.pizzaPrice,
             "pizzaSize": self.pizzaSize
         }
+    
     def update(self, pizza="", pizzaPrice="", pizzaSize=""):
        """only updates values with length"""
        if len(pizza) > 2:
@@ -76,6 +77,7 @@ class Pizzas(db.Model):
        if len(pizzaSize) > 0:
            self.pizzaSize = pizzaSize
        db.session.commit()
+       db.session.add(self)
        return self
     
     def delete(self):
@@ -88,17 +90,17 @@ def initPizzas():
     with app.app_context():
         db.create_all()
 
-        # Test data
         u1 = Pizzas(pizza='Cheese pizza', pizzaPrice='14.99', pizzaSize='Large')
         u2 = Pizzas(pizza='Cheese pizza', pizzaPrice='12.99', pizzaSize='Medium')
         u3 = Pizzas(pizza='Cheese pizza', pizzaPrice='10.99', pizzaSize='Small')
         
+        pizzas = [u1, u2, u3]
 
-pizza = [u1, u2, u3]
+        for pizza in pizzas:
+            try:
+                object = pizza.create()
+                print(f"Created new uid {object.pizza}")
+            except:
+                print(f"Records exist uid {pizza.pizza}, or error.")
 
-for pizza in Pizzas:
-    try:
-        pizza.create()
-    except IntegrityError:
-        db.session.remove()
-        print(f"This is a duplicate")
+initPizzas()
