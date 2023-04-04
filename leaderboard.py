d12841f66c769b01b3ed635534c50c489599f22d
Gene Chang <genechang0219@gmail.com>
2023-04-03 23:54:42 -0700

    leaderboard

---
+++ leaderboard.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource
+from datetime import datetime
+from model_chess import getUser, getName
+
+leaderboard_api = Blueprint('leaderboard_api', __name__, url_prefix='/api/leaderboard')
+api = Api(leaderboard_api)
+
+# example leaderboard data
+leaderboard = {
+    'players': [
+        {'name': 'John', 'score': 10},
+        {'name': 'Emily', 'score': 8},
+        {'name': 'Michael', 'score': 6},
+        {'name': 'Jane', 'score': 4},
+        {'name': 'David', 'score': 2}
+    ]
+}
+
+class LeaderboardAPI:
+    class _Read(Resource):
+        def get(self):
+            return leaderboard
+
+    class _AddScore(Resource):
+        def post(self):
+            # read data from json body
+            body = request.get_json()
+            name = body.get('name')
+            score = body.get('score')
+
+            # check if name and score are present in request
+            if not name or not score:
+                return {'message': 'Please provide a name and a score'}, 400
+            
+            # check if score is valid (must be a positive integer)
+            try:
+                score = int(score)
+                if score < 0:
+                    raise ValueError
+            except ValueError:
+                return {'message': 'Score must be a positive integer'}, 400
+            
+            # add player to leaderboard
+            leaderboard['players'].append({'name': name, 'score': score})
+            
+            # sort leaderboard by score in descending order
+            leaderboard['players'] = sorted(leaderboard['players'], key=lambda p: p['score'], reverse=True)
+            
+            return {'message': f'{name} added to leaderboard'}, 201
+
+    # building RESTapi endpoint
+    api.add_resource(LeaderboardAPI._Read, '/')
+    api.add_resource(LeaderboardAPI._AddScore, '/add_score')
+
---
+++ robot.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource # used for REST API building
+from datetime import datetime
+from users import test
+from __init__ import db, app
+pizza_api = Blueprint('robot_api', __name__,
+                   url_prefix='/api/robot')
+
+api = Api(pizza_api)
+
+class PizzaAPI:
+    class _Create(Resource):
+        def post(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+                        
+            ''' Avoid garbage in, error checking '''
+            # validate name
+            pizzaType = body.get('pizzaType')
+            pizzaSize = body.get('pizzaSize')
+            pizzaPrice = body.get('pizzaPrice')
+
+            if pizzaType is None or len(pizzaType) < 2:
+                return {'message': f'Pizzatype is missing or too short'}, 210
+            
+            uo = Pizzas(pizza=pizza)
