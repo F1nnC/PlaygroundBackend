487f47d75b18f3b7087dfc1b50f8f9a481e9a14f
zeen <zeen6808005293@gmail.com>
2023-04-06 13:32:20 -0700

    added sqlite table

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ from flask import render_template  # import render_template from "public" flask 
 
 # import "packages" from "this" project
 from __init__ import app,db  # Definitions initialization
-from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
 
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
+
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 
@@ -43,7 +41,7 @@ def stub():
 @app.before_first_request
 def activate_job():  # activate these items 
     db.init_app(app)
-    initJokes()
+
     initUsers()
     initPlayers()
 
