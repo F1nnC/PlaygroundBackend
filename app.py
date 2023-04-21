e55f0265967276304731a2018dd14a952c49b298
Gene Chang <genechang0219@gmail.com>
2023-04-21 01:31:56 -0700

    d

--- app.py
+++ app.py
@@ -7,15 +7,26 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///leaderboard.db'
 db = SQLAlchemy(app)
 
+
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     level = db.Column(db.Integer, nullable=False)
 
+
 player_leaderboard_api = Blueprint('player_leaderboard_api', __name__, url_prefix='/api')
 api = Api(player_leaderboard_api)
 
+
 class PlayerAPI(Resource):
+    def get(self):
+        players = Player.query.all()
+        result = []
+        for player in players:
+            player_data = {'name': player.name, 'level': player.level}
+            result.append(player_data)
+        return jsonify(result)
+
     def post(self):
         data = request.json
         player = Player(name=data['name'], level=data['level'])
@@ -34,9 +45,9 @@ class PlayerAPI(Resource):
             return jsonify({'error': 'Player not found.'}), 404
 
 
+api.add_resource(PlayerAPI, '/', endpoint='leaderboard')
 api.add_resource(PlayerAPI, '/win', endpoint='win')
 
-api.add_resource(PlayerAPI, '/', endpoint='leaderboard')
 
 def create_testing_data():
     with app.app_context():
@@ -53,9 +64,11 @@ def create_testing_data():
             db.session.rollback()
             print(f"Records exist, duplicate email, or error: {u1.id}")
 
+
 @app.before_first_request
 def activate_job():
     create_testing_data()
+
 
 @app.route('/view-db')
 def view_db():
@@ -66,11 +79,13 @@ def view_db():
         result.append(player_data)
     return jsonify(result)
 
+
 app.register_blueprint(player_leaderboard_api, url_prefix='/api')
 
+
 if __name__ == "__main__":
-    # change name for testing
     from flask_cors import CORS
+
     cors = CORS(app)
     app.run(debug=True, host="0.0.0.0", port=5000)
 
