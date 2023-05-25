e389d1cd4adeff0119ff14a935bfbc4e3a7f68e4
Gene Chang <genechang0219@gmail.com>
2023-05-24 22:58:48 -0700

    d

--- pizzaOrders.py
+++ pizzaOrders.py
@@ -146,7 +146,7 @@ def getAddress(address):
 def createTestingData1():
     with app.app_context():
         db.create_all()
-        u1 = Order(orderName ="Balls", uid="100", pizzaType = "Cheese", address = "2305 Nighthawk lane")  
+        u1 = Order(orderName ="Balls", pizzaType = "Cheese", address = "2305 Nighthawk lane")  
         try:
             '''add user/note data to table'''
             u1.create()
