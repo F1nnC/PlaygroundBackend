60ad5d18eca06b4b6b4be26af4328b692eb27eb2
Gene Chang <genechang0219@gmail.com>
2023-05-26 15:09:48 -0700

    sqlalchemy needs to eat an app

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ dbURI = 'sqlite:///volumes/sqlite.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
 app.config['SECRET_KEY'] = 'SECRET_KEY'
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 Migrate(app, db)
 
 # Images storage
