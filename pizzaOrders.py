d0c82d0c4b5ab920e188e8e747431c4829a983ab
Gene Chang <genechang0219@gmail.com>
2023-05-28 13:32:41 -0700

    this will maybe??? break this

--- pizzaOrders.py
+++ pizzaOrders.py
@@ -1,29 +1,26 @@
-import os, json
-from __init__ import db, app
-from sqlalchemy.exc import IntegrityError
-
+import os
+import json
 from flask_login import UserMixin
 from flask import Blueprint, request, jsonify
-from flask import jsonify
-
-
+from sqlalchemy.exc import IntegrityError
+from __init__ import db, app
 
 class Order(UserMixin, db.Model):
     __tablename__ = 'PizzaOrders'
     id = db.Column(db.Integer, primary_key=True)
-    _orderName = db.Column(db.String(255), unique=False, nullable=False)
-    _pizzaType = db.Column(db.String(255), unique=False, nullable=False)
-    _address = db.Column(db.String(255), unique=False, nullable=False)
-    _uid = db.Column(db.Integer, unique=False, nullable=False)
+    orderName = db.Column(db.String(255), unique=False, nullable=False)
+    pizzaType = db.Column(db.String(255), unique=False, nullable=False)
+    address = db.Column(db.String(255), unique=False, nullable=False)
+    uid = db.Column(db.Integer, unique=False, nullable=False)
 
-    def __init__(self, orderName="", uid="0", pizzaType="", address=""):
-        self._uid = uid
-        self._orderName = orderName
-        self._pizzaType = pizzaType
-        self._address = address
+    def __init__(self, orderName="", uid=0, pizzaType="", address=""):
+        self.uid = uid
+        self.orderName = orderName
+        self.pizzaType = pizzaType
+        self.address = address
 
     def __repr__(self):
-        return f"Order({self._uid}, {self._orderName}, {self._pizzaType}, str{self._address})"
+        return f"Order({self.uid}, {self.orderName}, {self.pizzaType}, {self.address})"
 
     def create(self):
         try:
@@ -36,51 +33,20 @@ class Order(UserMixin, db.Model):
 
     def read(self):
         return {
-            "uid": self._uid,
-            "name": self._orderName,
-            "pizzaType": self._pizzaType,
-            "address": self._address
+            "uid": self.uid,
+            "name": self.orderName,
+            "pizzaType": self.pizzaType,
+            "address": self.address
         }
-
-    @property
-    def orderName(self):
-        return self._orderName
-
-    @orderName.setter
-    def orderName(self, orderName):
-        self._orderName = orderName
-
-    @property
-    def address(self):
-        return self._address
-
-    @address.setter
-    def address(self, address):
-        self._address = address
-
-    def is_address(self, address):
-        return self._address == address
-
-    @property
-    def pizzaType(self):
-        return self._pizzaType
-
-    @pizzaType.setter
-    def pizzaType(self, pizzaType):
-        self._pizzaType = pizzaType
-
-    def __str__(self):
-        return json.dumps(self.read())
 
     def update(self, orderName="", pizzaType="", address=""):
         if orderName:
-            self._orderName = orderName
+            self.orderName = orderName
         if pizzaType:
-            self._pizzaType = pizzaType
+            self.pizzaType = pizzaType
         if address:
-            self._address = address
+            self.address = address
         db.session.commit()
-        db.session.add(self)
         return self
 
     def delete(self):
@@ -88,77 +54,49 @@ class Order(UserMixin, db.Model):
         db.session.commit()
         return None
 
-    def get_id(self):
-        return self._uid
-
-    def get_name(self):
-        return self._orderName
-
-    def get_type(self):
-        return self._pizzaType
-
-    def get_address(self):
-        return self._address
-
-
 def getUser(uid):
-    users = Order.query.all()
-    for user in users:
-        if user.get_id() == uid:
-            return user
+    user = Order.query.filter_by(uid=uid).first()
+    if user:
+        return user
     return "Invalid user"
 
-
 def getName(orderName):
-    users = Order.query.all()
-    for user in users:
-        if user.get_name() == orderName:
-            return user
+    user = Order.query.filter_by(orderName=orderName).first()
+    if user:
+        return user
     return None
-
 
 def make_id():
     users = Order.query.all()
     uid = 0
     for user in users:
-        if user.get_id() > uid:
-            uid = user.get_id()
+        if user.uid > uid:
+            uid = user.uid
     if uid < 100:
         return 100
     return uid + 1
 
 def getType(pizzaType):
-    users = Order.query.all()
-    for user in users:
-        if(user.get_type() == pizzaType):
-            return user
-    else: 
-        return None
-
+    user = Order.query.filter_by(pizzaType=pizzaType).first()
+    if user:
+        return user
+    return None
 
 def getAddress(address):
-    users = Order.query.all()
-    for user in users:
-        if(user.get_address() == address):
-            return user
-    else: 
-        return None
+    user = Order.query.filter_by(address=address).first()
+    if user:
+        return user
+    return None
 
 def createTestingData1():
     with app.app_context():
         db.create_all()
-        u1 = Order(orderName ="Balls", pizzaType = "Cheese", address = "2305 Nighthawk lane")  
+        u1 = Order(orderName="Balls", pizzaType="Cheese", address="2305 Nighthawk lane")
         try:
-            '''add user/note data to table'''
             u1.create()
-            # u5.create()
-            # u6.create()
         except IntegrityError:
-            '''fails with bad or duplicate data'''
             db.session.remove()
             print(f"Records exist, duplicate email, or error: {u1.uid}")
-
-
 
 if __name__ == "__main__":
     createTestingData1()
