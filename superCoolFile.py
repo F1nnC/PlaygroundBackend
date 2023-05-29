e4b262b5aa8177a09b9e09e80f7c08e932f2fe66
F1nnC <captainsurfervideos@gmail.com>
2023-05-29 16:20:39 -0700

    added comments

--- superCoolFile.py
+++ superCoolFile.py
@@ -16,36 +16,49 @@ pizza_user_api = Blueprint('pizza_user_api', __name__,
                    
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
+
 api = Api(pizza_user_api)
-class UserAPI:        
+
+class UserAPI:
+    
     class _Create(Resource):
         def put(self):
+            # Retrieve JSON data from the request body
             body = request.get_json(force=True)
             name = body.get('name')
             score = 1  # Increase the score by 1
+            
+            # Query PizzaUsers table for users with names 'Toby' or 'Gene'
             users = PizzaUsers.query.filter(PizzaUsers.name.in_(['Toby', 'Gene'])).all()
+            
+            # Get user by name
             user = getName(name)
+            
             if user:
                 for user in users:
+                    # Update the user's score
                     updated_score = user.update_score(score)
                 return {'message': f'Score updated to {updated_score}'}
             else:
                 return {'message': f'User {name} not found'}, 404
+        
         def post(self):
-            ''' Read data for json body '''
+            ''' Read data from JSON body '''
             body = request.get_json()
             
             ''' Avoid garbage in, error checking '''
-            # validate name
             
+            # Validate name
             name = body.get('name')
             if name is None or len(name) < 2:
-                return {'message': f'Name is missing, or is less than 2 characters'}, 210
-            # validate uid
+                return {'message': f'Name is missing or is less than 2 characters'}, 210
+            
+            # Validate uid
             # uid = body.get('uid')
             # if uid is None or len(uid) < 2:
-            #     return {'message': f'User ID is missing, or is less than 2 characters'}, 210
-            # look for password and dob
+            #     return {'message': f'User ID is missing or is less than 2 characters'}, 210
+            
+            # Look for password and dob
             password = body.get('password')
             dob = body.get('dob')
 
@@ -53,52 +66,63 @@ class UserAPI:
             uo = PizzaUsers(name=name)
             
             ''' Additional garbage error checking '''
-            # set password if provided
+            
+            # Set password if provided
             if password is not None:
                 uo.set_password(password)
-            # convert to date type
+            
+            # Convert dob to date type
             if dob is not None:
                 try:
                     uo.dob = dob
                 except:
                     return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 210
             
-            # create user in database
+            # Create user in the database
             user = uo.create()
-            # success returns json of user
+            
+            # Success returns JSON of user
             if user:
                 return jsonify(uo.read())
-            # failure returns error
+            
+            # Failure returns error
             return {'message': f'{name} is duplicate'}, 210
 
     class _Read(Resource):
         def get(self):
+            # Get all users from the PizzaUsers table
             users = PizzaUsers.query.all()
             
-            # Bubble sort yay!!!!!!!
+            # Bubble sort users based on score (descending order)
             n = len(users)
             for i in range(n - 1):
                 for j in range(0, n - i - 1):
                     if users[j].score < users[j + 1].score:
                         users[j], users[j + 1] = users[j + 1], users[j]
             
+            # Prepare users in JSON format
             json_ready = [user.read() for user in users]
+            
             return jsonify(json_ready)
-
-
 
     class _DeleteGame(Resource):
         def post(self):
             body = request.get_json(force=True)
             name = body.get('name')
             date = body.get('date')
+            
+            # Get user by name
             user = getName(name)
+            
             return user.deleteGame(date)
 
     class _UpdateScore(Resource):
         def put(self, uid):
+            # Get user by uid
             user = getUser(uid)
+            
             if user != "Invalid user":
+                # Update the user's score
                 user.score += 1
                 return {'message': 'Score updated successfully'}
             else:
@@ -110,13 +134,19 @@ class UserAPI:
             name = body.get('name')
             password = body.get('password')
             score = 1  
+            
+            # Get user by name
             user = getName(name)
+            
             if user and user.is_password_match(password):
+                # Update the user's score
                 updated_score = user.update_score(score)
                 return {'message': f'Score updated to {updated_score}'}
             elif not user:
+                # Create a new user if the user doesn't exist
                 uo = PizzaUsers(name=name, password=password, score=score)
                 created_user = uo.create()
+                
                 if created_user:
                     return {'message': 'New user created successfully'}
                 else:
@@ -129,15 +159,19 @@ class UserAPI:
             body = request.get_json(force=True)
             names = body.get('names', [])
             deleted_users = []
+            
             for name in names:
+                # Get user by name
                 user = getName(name)
+                
                 if user:
+                    # Delete the user
                     user.delete()
                     deleted_users.append(name)
+            
             return {'message': 'Deleted users', 'deleted_users': deleted_users}
 
-
-    # building RESTapi endpoint
+    # Building REST API endpoints
     api.add_resource(_Create, '/create')
     api.add_resource(_Read, '/')
     api.add_resource(_DeleteGame, '/delete_game')
@@ -147,3 +181,4 @@ class UserAPI:
 
 if __name__ == "__main__":
     app.run()
+
