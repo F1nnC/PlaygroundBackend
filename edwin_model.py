c8ff2ae5602d623fcd440fe0e3bc1c6a1829c696
EdwinKuttappi <edwinab0007@gmail.com>
2023-06-05 12:38:18 -0700

    model

--- edwin_model.py
+++ edwin_model.py
@@ -18,12 +18,12 @@ class Phone(db.Model):
     _company = db.Column(db.String(255), unique=False, nullable=False) #_name
     _model = db.Column(db.String(255), unique=True, nullable=False) #_uid
     _price = db.Column(db.String(255), unique=False, nullable=False) # _password
-    # the compa
+    # the variables are being detected and set equal to the parameter
     def __init__(self, company, model, price):
         self._company = company
         self._model = model
         self._price = price
-
+    # This is the setter for the company
     @property
     def company(self):
         return self._company
@@ -39,7 +39,7 @@ class Phone(db.Model):
     @model.setter
     def model(self, model):
         self._model = model
-        
+    # Function made to call the model so that users can check if the model is present
     def is_model(self, model):
         return self._model == model
     
@@ -62,7 +62,7 @@ class Phone(db.Model):
         except IntegrityError:
             db.session.remove()
             return None
-
+    # This is for the reading values on the table
     def read(self):
         return {
             "id": self.id,
@@ -70,7 +70,7 @@ class Phone(db.Model):
             "model": self.model,
             "price": self.price,
         }
-
+    # Updating the values in the database with the parameters
     def update(self, company="", model="", price=""):
         """only updates values with length"""
         if len(company) > 0:
@@ -82,12 +82,12 @@ class Phone(db.Model):
         db.session.commit()
         db.session.add(self)
         return self
-
+    # The delete function
     def delete(self):
         db.session.delete(self)
         db.session.commit()
         return None
-
+# The function that creates all of the values in the database
 def initPhones():
     with app.app_context():
         """Create database and tables"""
@@ -99,30 +99,33 @@ def initPhones():
         p4 = Phone(company='LG', model='Wing', price='999')
         p5 = Phone(company='Motorola', model='Razr', price='1399')
         p6 = Phone(company='Google', model='Pixel 7', price='599')
-
+        # Simple list to reference in other code
         phones = [p1, p2, p3, p4, p5, p6]
 
         """Builds sample user/note(s) data"""
         for phone in phones:
             try:
+                # Try to add the phone
                 '''add user to table'''
                 object = phone.create()
                 print(f"Created new uid {object.model}")
             except:
+                # Checks if records exist
                 '''fails with bad or duplicate data'''
                 print(f"Records exist uid {phone.model}, or error.")
                 
 
+# Helps the user to find the company
 def find_by_company(company):
     with app.app_context():
         name = Phone.query.filter_by(_company=company).first()
     return name
-
+# Creates the Phone, Company, and Price
 def create():
     phone = input("Enter the Phone: ")
     company = input("Enter the manufacturer: ")
     price = input("Enter the Price: ")
-
+    # Phone list referencing the class from before
     phone = Phone(phone=phone, 
                 company=company, 
                 price=price,
@@ -134,7 +137,7 @@ def create():
                 print("Created\n", object.read())
             except:
                 print("Unknown error uid {uid}")
-
+# The read function
 def read():
     with app.app_context():
         table = Phone.query.all()
